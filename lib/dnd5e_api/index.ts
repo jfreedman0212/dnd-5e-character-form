cce5d627e9215159bcba6ad648e265500a796b3e
Josh Freedman <joshfreedman@pm.me>
2021-04-24 09:48:28 -0400

    add some basic types reformat tsconfig

---
+++ lib/dnd5e_api/index.ts
@@ -0,0 +1,40 @@
+export type ApiReference = Readonly<{
+    index: string,
+    name: string,
+    url: string
+}>;
+
+export type ResourceList = Readonly<{
+    count: number,
+    results: ApiReference[]
+}>;
+
+export type Choice = Readonly<{
+    choose: number,
+    type: string,
+    from: ApiReference[]
+}>;
+
+export type AbilityBonus = Readonly<{
+    bonus: number,
+    ability_score: ApiReference
+}>;
+
+export type Race = Readonly<{
+    index: string,
+    name: string,
+    speed: number,
+    ability_bonuses: AbilityBonus[],
+    alignment: string,
+    age: string,
+    size: string,
+    size_description: string,
+    starting_proficiencies: ApiReference[],
+    languages: ApiReference[],
+    // language_options?
+    language_desc: string,
+    traits: ApiReference[],
+    // trait_options?
+    subraces: ApiReference[],
+    url: string
+}>;
