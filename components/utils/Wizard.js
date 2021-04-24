5ac72fd414b9c27425af0db405440d61c51cc01d
Josh Freedman <joshfreedman@pm.me>
2021-04-23 23:54:06 -0400

    group fields in wizard by step refactor wizard to use reducer instead of state

--- components/utils/Wizard.js
+++ components/utils/Wizard.js
@@ -1,26 +1,43 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "GO_FORWARD":
+            return {
+                ...state,
+                wizardData: {
+                    ...state.wizardData,
+                    [state.steps[state.currentStep].name]: action.payload
+                },
+                currentStep:
+                    state.currentStep < state.steps.length - 1
+                        ? state.currentStep + 1
+                        : state.currentStep
+            };
+        case "GO_BACK":
+            return {
+                ...state,
+                currentStep:
+                    state.currentStep > 0
+                        ? state.currentStep - 1
+                        : state.currentStep
+            };
+    }
+}
 
 export default function Wizard({ steps }) {
-    const [currentStep, setCurrentStep] = useState(0);
-    const [wizardData, setWizardData] = useState({});
+    const [{ currentStep, wizardData }, dispatch] = useReducer(reducer, {
+        steps,
+        currentStep: 0,
+        wizardData: {}
+    });
 
     function goForward(data) {
-        setWizardData((oldData) => ({ ...oldData, ...data }));
-        setCurrentStep((step) => {
-            if (step < steps.length - 1) {
-                return step + 1;
-            }
-            return step;
-        });
+        dispatch({ type: "GO_FORWARD", payload: data });
     }
 
     function goBack() {
-        setCurrentStep((step) => {
-            if (step > 0) {
-                return step - 1;
-            }
-            return step;
-        });
+        dispatch({ type: "GO_BACK" });
     }
 
     const Current = steps[currentStep].component;
