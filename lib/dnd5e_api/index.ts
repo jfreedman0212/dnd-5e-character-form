9b749420bf3a75a6f3dec7d4ee93050e4ef67703
Josh Freedman <joshfreedman@pm.me>
2021-04-25 15:23:02 -0400

    Make remaining form elements TS

--- lib/dnd5e_api/index.ts
+++ lib/dnd5e_api/index.ts
@@ -1,40 +1,51 @@
 export type ApiReference = Readonly<{
-    index: string,
-    name: string,
-    url: string
+    index: string;
+    name: string;
+    url: string;
 }>;
 
 export type ResourceList = Readonly<{
-    count: number,
-    results: ApiReference[]
+    count: number;
+    results: ApiReference[];
 }>;
 
 export type Choice = Readonly<{
-    choose: number,
-    type: string,
-    from: ApiReference[]
+    choose: number;
+    type: string;
+    from: ApiReference[];
 }>;
 
 export type AbilityBonus = Readonly<{
-    bonus: number,
-    ability_score: ApiReference
+    bonus: number;
+    ability_score: ApiReference;
 }>;
 
 export type Race = Readonly<{
-    index: string,
-    name: string,
-    speed: number,
-    ability_bonuses: AbilityBonus[],
-    alignment: string,
-    age: string,
-    size: string,
-    size_description: string,
-    starting_proficiencies: ApiReference[],
-    languages: ApiReference[],
+    index: string;
+    name: string;
+    speed: number;
+    ability_bonuses: AbilityBonus[];
+    alignment: string;
+    age: string;
+    size: string;
+    size_description: string;
+    starting_proficiencies: ApiReference[];
+    languages: ApiReference[];
     // language_options?
-    language_desc: string,
-    traits: ApiReference[],
+    language_desc: string;
+    traits: ApiReference[];
     // trait_options?
-    subraces: ApiReference[],
-    url: string
+    subraces: ApiReference[];
+    url: string;
+}>;
+
+export type Trait = Readonly<{
+    index: string;
+    races: ApiReference[];
+    subraces: ApiReference[];
+    name: string;
+    desc: string[];
+    proficiencies: ApiReference[];
+    proficiency_choices: Choice[];
+    url: string;
 }>;
